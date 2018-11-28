4753a5a390a2a4ea411580a894a3bfb5ea04a89c
andy <andy.niu@outlook.com>
2018-11-28 15:56:37 +0800

    添加base64加密解密

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,11 @@
     });
     $('#convert2html').on('click', function () {
         var code = $('#code').val()
-        var result = code.split('\n').filter(function (item) { return item != '' }).map(function (item) { return '<p>' + item.replace(/(^\s*)|(\s*$)/gi, '') + '</p>' }).join('\n')
+        var result = code.split('\n').filter(function (item) {
+            return item != ''
+        }).map(function (item) {
+            return '<p>' + item.replace(/(^\s*)|(\s*$)/gi, '') + '</p>'
+        }).join('\n')
 
         $('#coderesult').val(result);
     });
@@ -35,7 +39,7 @@
         }
     };
     //样式格式化
-    $('#fromart_button').on('click', () => {        
+    $('#fromart_button').on('click', () => {
         $('#coderesult').val(lCSSCoder.format($('#code').val()));
     });
     //样式压缩
@@ -45,37 +49,61 @@
     //\n替换逗号
     $('#replace_button').on('click', () => {
         var code = $('#code').val()
-        let array = code.split('\n').filter(function (item) { return item.replace(/(^\s*)|(\s*$)/gi, '') != '' }).map(function (item) { return item.replace(/(^\s*)|(\s*$)/gi, '') })
+        let array = code.split('\n').filter(function (item) {
+            return item.replace(/(^\s*)|(\s*$)/gi, '') != ''
+        }).map(function (item) {
+            return item.replace(/(^\s*)|(\s*$)/gi, '')
+        })
         $('#coderesult').val(array.join(','));
     });
     //UrlEncode
     $('#encode_button').on('click', () => {
-        $('#coderesult').val(encodeURIComponent($('#code').val()).replace(/'/g, "%27").replace(/"/g, "%22"));
-        ;
+        $('#coderesult').val(encodeURIComponent($('#code').val()).replace(/'/g, "%27").replace(/"/g, "%22"));;
     });
     //UrlDecode
     $('#decode_button').on('click', () => {
         $('#coderesult').val(decodeURIComponent($('#code').val().replace(/\+/g, " ")));
     });
 
-    $('#coderesult')[0].addEventListener("dragenter", function(event) { event.preventDefault(); }, false);
-    $('#coderesult')[0].addEventListener("dragover", function(event) { event.preventDefault(); }, false);
-    $('#coderesult')[0].addEventListener("drop", function(event) {
+    //Base64 Encode
+    $('#bencode_button').on('click', () => {
+        let base64 = new Base64();
+        $('#coderesult').val(base64.encode($('#code').val()));
+    });
+    //Base64 Decode
+    $('#bdecode_button').on('click', () => {
+        let base64 = new Base64();
+        $('#coderesult').val(base64.decode($('#code').val()));
+    });
+
+    $('#coderesult')[0].addEventListener("dragenter", function (event) {
+        event.preventDefault();
+    }, false);
+    $('#coderesult')[0].addEventListener("dragover", function (event) {
+        event.preventDefault();
+    }, false);
+    $('#coderesult')[0].addEventListener("drop", function (event) {
         var reader = new FileReader();
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             $('#coderesult')[0].innerHTML = e.target.result;
         };
         reader.readAsDataURL(event.dataTransfer.files[0]);
         event.preventDefault();
     }, false);
 
-    var tips = function(msg){
+    var tips = function (msg) {
         $('.copyresult').show().text(msg).addClass('active');
-        var t = setTimeout(()=>{$('.copyresult').removeClass('active');clearTimeout(t); setTimeout(()=>{$('.copyresult').hide('active');},500) },1500);
+        var t = setTimeout(() => {
+            $('.copyresult').removeClass('active');
+            clearTimeout(t);
+            setTimeout(() => {
+                $('.copyresult').hide('active');
+            }, 500)
+        }, 1500);
     }
 
-    $('#coderesult').on('dblclick ',function(){
-        if(this.value.replace(/(^\s*)|(\s*$)/gi, '') == '') return
+    $('#coderesult').on('dblclick ', function () {
+        if (this.value.replace(/(^\s*)|(\s*$)/gi, '') == '') return
         var textArea = document.createElement("textarea");
         textArea.style.position = 'fixed';
         textArea.style.top = '0';
@@ -99,6 +127,110 @@
             tips('该浏览器不支持点击复制到剪贴板')
         }
         document.body.removeChild(textArea);
-    })
+    });
+
+    function Base64() {
+
+        // private property
+        _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+        // public method for encoding
+        this.encode = function (input) {
+            var output = "";
+            var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+            var i = 0;
+            input = _utf8_encode(input);
+            while (i < input.length) {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+                output = output +
+                    _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+                    _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+            }
+            return output;
+        }
+
+        // public method for decoding
+        this.decode = function (input) {
+            var output = "";
+            var chr1, chr2, chr3;
+            var enc1, enc2, enc3, enc4;
+            var i = 0;
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+            while (i < input.length) {
+                enc1 = _keyStr.indexOf(input.charAt(i++));
+                enc2 = _keyStr.indexOf(input.charAt(i++));
+                enc3 = _keyStr.indexOf(input.charAt(i++));
+                enc4 = _keyStr.indexOf(input.charAt(i++));
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+                output = output + String.fromCharCode(chr1);
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+            }
+            output = _utf8_decode(output);
+            return output;
+        }
+
+        // private method for UTF-8 encoding
+        _utf8_encode = function (string) {
+            string = string.replace(/\r\n/g, "\n");
+            var utftext = "";
+            for (var n = 0; n < string.length; n++) {
+                var c = string.charCodeAt(n);
+                if (c < 128) {
+                    utftext += String.fromCharCode(c);
+                } else if ((c > 127) && (c < 2048)) {
+                    utftext += String.fromCharCode((c >> 6) | 192);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                } else {
+                    utftext += String.fromCharCode((c >> 12) | 224);
+                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
+
+            }
+            return utftext;
+        }
+
+        // private method for UTF-8 decoding
+        _utf8_decode = function (utftext) {
+            var string = "";
+            var i = 0;
+            var c = c1 = c2 = 0;
+            while (i < utftext.length) {
+                c = utftext.charCodeAt(i);
+                if (c < 128) {
+                    string += String.fromCharCode(c);
+                    i++;
+                } else if ((c > 191) && (c < 224)) {
+                    c2 = utftext.charCodeAt(i + 1);
+                    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                    i += 2;
+                } else {
+                    c2 = utftext.charCodeAt(i + 1);
+                    c3 = utftext.charCodeAt(i + 2);
+                    string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                    i += 3;
+                }
+            }
+            return string;
+        }
+    }
 
 })($, window);
